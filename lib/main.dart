1400154a30e0a36716ff2776a27841d21cd62978
gurasan0110 <gurasan0110@icloud.com>
2025-01-26 21:21:55 +0900

    Implement splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+Future<void> main() async {
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  // 2秒間表示
+  await Future.delayed(Duration(seconds: 2));
+
+  FlutterNativeSplash.remove();
+
   runApp(const MainApp());
 }
 
