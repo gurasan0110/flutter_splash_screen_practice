28a99d0a24925a0e8bdf24bc3212ee805bdc55d0
gurasan0110 <gurasan0110@icloud.com>
2025-01-26 20:30:17 +0900

    Create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_splash_screen_practice
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/gurasan0110/flutter_splash_screen_practice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gurasan0110.flutter_splash_screen_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
